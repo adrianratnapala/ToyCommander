a2f634bb9c84adac5a63b8fb0a56522dd80a840e
Adrian Ratnapala <adrian.ratnapala@gmail.com>
2010-10-31 14:48:06 +0000

    Old panes now contain the prompt as well as the command.
    A consequence of this was also a shakeup of the stylesheet.  Look at the diff.

--- commander.js
+++ commander.js
@@ -37,18 +37,28 @@ function requestURI(receiver, uri, gotit) {
 
 // Runs the string "text", returns an DIV (pane) to catch the results. 
 function runCommand( session, text, gotit_cb ) {
-        // create a pane to return results into. 
-        var pane     = document.createElement('div');
-        pane.setAttribute('class', 'pane');
+        var prspan = document.createElement('span');
+        prspan.setAttribute( 'class', 'oprompt' );
+        prspan.appendChild( createPrompt(session) );
+
+        var input = document.createElement('span');
+        input.setAttribute('class','input');
+        input.appendChild( document.createTextNode(text) );
+ 
+        var command = document.createElement('div');
+        command.setAttribute('class', 'ocommand');
+        command.appendChild( prspan );
+        command.appendChild( input );
+
         var receiver = document.createElement('div');
         receiver.setAttribute('class', 'response');
 
-        var command = document.createElement('div');
-        command.setAttribute('class', 'command');
-       
-        command.appendChild( document.createTextNode(text) );
+        // create a pane to return results into. 
+        var pane     = document.createElement('div');
+        pane.setAttribute('class', 'pane');
         pane.appendChild( command );
         pane.appendChild( receiver );
+
         
         function gotit() {
                 if(gotit_cb)
@@ -65,11 +75,13 @@ function runCommand( session, text, gotit_cb ) {
 }
 
 function createPrompt(session) {
-        return document.createTextNode(++session.command_id + '$ ');
+        return document.createTextNode(session.command_id + '$ ');
 }
 
 // Create user's command line input widget for session.
 function commandInput(session) {
+        ++session.command_id;
+
         var prspan = document.createElement('span');
         var prompt = createPrompt(session)
         prspan.setAttribute( 'class', 'prompt' );
